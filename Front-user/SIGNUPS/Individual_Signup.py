6e124e10ffe43607cfcf805d26490bacb97b229a
mibrahimrehman <mibrahim@akru.co>
2022-09-01 14:05:20 +0500

    ok

--- Front-user/SIGNUPS/Individual_Signup.py
+++ Front-user/SIGNUPS/Individual_Signup.py
@@ -450,6 +450,7 @@ class test_invite(unittest.TestCase):
             raise Exception
 
         time.sleep(2)
+        #comment
         try:
             magicButtonFromSignupModal=wait.until(EC.element_to_be_clickable((By.XPATH,'//div[@class="donwload-btn"]')))
             magicButtonFromSignupModal.click()
