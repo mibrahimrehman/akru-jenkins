d43247adcb5fedeedc3e47f09f4846c13798bea9
mibrahimrehman <mibrahim@akru.co>
2022-09-06 12:25:00 +0500

    ok

--- email_api/prac.py
+++ email_api/prac.py
@@ -1,27 +1,46 @@
+from pickle import FALSE
+import unittest
+
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
+import time
+import names
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+import os
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 import allure
 
-import time
+class test_invite(unittest.TestCase):
 
-chrome_options = Options()
+    def setUp(self):
+        WINDOW_SIZE = "1920,1080"
+        chrome_options = Options()
         
-chrome_options.add_argument("--headless")
-#browser exposes an executable file
-#Through Selenium test we will invoke the executable file which will then #invoke actual browser
-service = ChromeService(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service , options=chrome_options)
-action = ActionChains (driver)
-#driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
-# to maximize the browser window
-driver.maximize_window()
+        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--start-maximized')
+        chrome_options.add_argument('--disable-setuid-sandbox')
+        chrome_options.add_argument('--remote-debugging-port=9222')
+        chrome_options.add_argument('--disable-dev-shm-using')
+        # s = Service('/home/ubuntu/script/pipeline/test/chromdriver/chromedriver')
+        #s = Service('/Users/qualityassurance/Desktop/automation-scripts/AVAXDEV_SHAHWAR/chromedriver')
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service , options=chrome_options)
+
+    def test_search_in_python_org(self):
+        self.driver.maximize_window()
+        driver =  self.driver
+        action = ActionChains (self.driver)
 #get method to launch the URL
-driver.get("https://postdev.akru.co/sign-up/czFYcldlTUVjMmFKbjF0dTVITXBfNjMxNmVlMGM1MWEzNDI4NmE5NDg2YzE5")
+        self.driver.get("https://postdev.akru.co/sign-up/czFYcldlTUVjMmFKbjF0dTVITXBfNjMxNmVlMGM1MWEzNDI4NmE5NDg2YzE5")
 #to refresh the browser
 #driver.refresh()
 # identifying the edit box and entering text in edit box
@@ -33,20 +52,20 @@ driver.get("https://postdev.akru.co/sign-up/czFYcldlTUVjMmFKbjF0dTVITXBfNjMxNmVl
 # # clears the content
 # driver.find_element(By.XPATH("//input[@name='key']")).clear()
 
-username_input = driver.find_element(By.XPATH, '//input[@name="zipCode"]')
-username_input.send_keys("00")
-time.sleep(3)
-action.key_down(Keys.COMMAND).perform()
-action.send_keys('a').perform()
-action.key_up(Keys.COMMAND).perform()
-action.send_keys(Keys.BACK_SPACE).perform()
-username_input.clear()
-#driver.find_element(By.XPATH, '//input[@name="zipCode"]').clear()
-time.sleep(3)
-driver.save_screenshot("prac.png")
-        #screenshot = Image.open("idsig.png")
-allure.attach.file(r"prac.png", "screenshot",attachment_type=allure.attachment_type.PNG)
-time.sleep(3)
-driver.quit()
-#to close the browser
-driver.close
+        username_input = driver.find_element(By.XPATH, '//input[@name="zipCode"]')
+        username_input.send_keys("00")
+        time.sleep(3)
+        action.key_down(Keys.COMMAND).perform()
+        action.send_keys('a').perform()
+        action.key_up(Keys.COMMAND).perform()
+        action.send_keys(Keys.BACK_SPACE).perform()
+        username_input.clear()
+        #driver.find_element(By.XPATH, '//input[@name="zipCode"]').clear()
+        time.sleep(3)
+        driver.save_screenshot("prac.png")
+                #screenshot = Image.open("idsig.png")
+        allure.attach.file(r"prac.png", "screenshot",attachment_type=allure.attachment_type.PNG)
+        time.sleep(3)
+        driver.quit()
+        #to close the browser
+        driver.close
