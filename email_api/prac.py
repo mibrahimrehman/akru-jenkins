d5ce88c0cc6840611345a7bca3cd08cf2f52e5cd
mibrahimrehman <mibrahim@akru.co>
2022-09-06 12:26:15 +0500

    ok

--- email_api/prac.py
+++ email_api/prac.py
@@ -22,7 +22,7 @@ class test_invite(unittest.TestCase):
         WINDOW_SIZE = "1920,1080"
         chrome_options = Options()
         
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
         chrome_options.add_argument('--no-sandbox')
