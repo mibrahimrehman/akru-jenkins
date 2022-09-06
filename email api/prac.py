e065457d384709f1238b1030bb1daf4b907ace48
mibrahimrehman <mibrahim@akru.co>
2022-09-06 12:06:53 +0500

    ok

--- email api/prac.py
+++ email api/prac.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+import allure
 
 import time
 #browser exposes an executable file
@@ -37,5 +38,10 @@ action.send_keys(Keys.BACK_SPACE).perform()
 username_input.clear()
 #driver.find_element(By.XPATH, '//input[@name="zipCode"]').clear()
 time.sleep(3)
+driver.save_screenshot("prac.png")
+        #screenshot = Image.open("idsig.png")
+allure.attach.file(r"prac.png", "screenshot",attachment_type=allure.attachment_type.PNG)
+time.sleep(3)
+driver.quit()
 #to close the browser
 driver.close
